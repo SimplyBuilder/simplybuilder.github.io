389d0d11ba9c1706cfbc83828c218c343fd22f65
JamilServices <jamilservicos@gmail.com>
2024-08-16 12:46:13 -0300

    🚀 feat(footer): add footer component with links to GitHub profiles

---
+++ institutional-website-v2/src/components/render/layout/footer/main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+import {language} from "@stores/language/main.js";
+
+const sloganStruct = () => language.get("canvas-footer-slogan");
+
+const struct = () => {
+    return {
+        element: "footer",
+        attr: {
+            class: "inline-flex flex-col flex-wrap w-full items-center justify-center py-6 px-4 font-mono"
+        },
+        children: [
+            {
+                element: "p",
+                attr: {
+                    class: "text-center text-sm md:text-base"
+                },
+                children: [
+                    {
+                        element: "span",
+                        text: sloganStruct()
+                    },
+                    {
+                        element: "span",
+                        children: [
+                            {
+                                element: "a",
+                                attr: {
+                                    class: "text-rose-600 dark:text-rose-400 hover:underline font-mono font-semibold tracking-wider",
+                                    href: "https://github.com/jamilservices",
+                                    target: "_blank"
+                                },
+                                text: "JamilServices"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                element: "div",
+                attr: {
+                    class: "mt-2 text-center"
+                },
+                children: [
+                    {
+                        element: "a",
+                        attr: {
+                            class: "text-rose-600 dark:text-rose-400 hover:underline font-mono font-semibold tracking-wider",
+                            href: "https://github.com/orgs/SimplyBuilder/repositories",
+                            target: "_blank"
+                        },
+                        text: "GitHub:SimplyBuilder",
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+export const footerStruct = Object.freeze(struct);
