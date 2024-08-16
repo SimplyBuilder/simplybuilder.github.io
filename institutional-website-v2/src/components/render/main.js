1bb2e4cfedcd91ee718c3f80a8c088c278e9df8d
JamilServices <jamilservicos@gmail.com>
2024-08-16 12:42:29 -0300

    ✨ feat(render): add main rendering logic for sections and canvas

---
+++ institutional-website-v2/src/components/render/main.js
@@ -0,0 +1,69 @@
+"use strict";
+
+import {CoreModule} from '@jamilservices/sb-core-module';
+import {NotifyModule} from '@jamilservices/sb-module-notify';
+
+import {canvasAndSections} from "@components/render/sections/main.js";
+import {layoutStruct} from "@components/render/layout/main.js";
+import {changeFlag} from "@components/render/utils/changeLanguage.js";
+
+const storeEvents = NotifyModule.instance("page-store");
+
+const renderSection = (section = {}) => {
+    const pages = canvasAndSections.sections;
+    const parent = CoreModule.getElementFromStore("main.canvas");
+    /**
+     * @type {HTMLElement}
+     */
+    const viewMain = CoreModule.getElementFromStore("view.main");
+    if (viewMain) {
+        /**
+         * remove elements and events recursively
+         */
+        CoreModule.removeElement(viewMain);
+    }
+    if (parent && pages[section]) {
+        const schema = {
+            parent,
+            struct: pages[section]()
+        };
+        CoreModule.createFromStruct(schema);
+    }
+};
+const renderCanvas = (section = {}) => {
+    CoreModule.createFromStruct({
+        parent: window.document.body,
+        struct: layoutStruct()
+    });
+    renderSection(section);
+};
+const renderPage = (section) => {
+    /**
+     * @type {HTMLElement}
+     */
+    const parent = CoreModule.getElementFromStore("main.canvas");
+    if (parent) {
+        renderSection(section);
+    } else renderCanvas(section);
+};
+const refreshPage = (section) => {
+    /**
+     * @type {HTMLElement}
+     */
+    const parent = CoreModule.getElementFromStore("canvas");
+    if (parent) CoreModule.removeElement(parent);
+    renderCanvas(section);
+};
+
+storeEvents.subscribe({
+    id: "render-watch",
+    fn: data => {
+        const {origin, render} = data;
+        if (origin === "page-store") return renderPage(render);
+        if (origin === "lang-store") return refreshPage(render);
+    }
+});
+
+CoreModule.eventRegister("flag-change.language", changeFlag);
+
+export default Object.freeze({});
