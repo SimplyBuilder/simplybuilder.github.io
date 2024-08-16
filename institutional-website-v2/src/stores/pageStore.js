f181760e19de5cdf4c471d8fb75f921c77bccd25
JamilServices <jamilservicos@gmail.com>
2024-08-16 12:41:32 -0300

    ✨ feat(pageStore): create page store with router and notify integration

---
+++ institutional-website-v2/src/stores/pageStore.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import { NotifyModule } from '@jamilservices/sb-module-notify';
+const storeEvents = NotifyModule.instance("page-store");
+const routerWacth = NotifyModule.instance("router");
+
+const store = {
+    page: undefined,
+    render: 'main'
+};
+
+const routerEvents = (data = {}) => {
+    try {
+        const {event, target} = data;
+        if (event === "router-update" && target) {
+            if(store.render === 'main') store.render = "section";
+            if(store.page !== target) {
+                store.page = target;
+                storeEvents.emit({
+                    origin: 'page-store', render: data.target
+                });
+            }
+        }
+        if (event === "lang-update") {
+            storeEvents.emit({
+                origin: 'lang-store', render: store.page
+            });
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+routerWacth.subscribe({
+    id: "store-watch",
+    fn: routerEvents
+});
+
+export default Object.freeze({});
