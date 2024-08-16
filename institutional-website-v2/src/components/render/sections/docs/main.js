ada8d506469cff731b1942abd78b095e5ebd9ad3
JamilServices <jamilservicos@gmail.com>
2024-08-16 12:44:28 -0300

    🎉 feat: add main documentation section structure

---
+++ institutional-website-v2/src/components/render/sections/docs/main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import {sidebarStruct} from "@components/render/sections/docs/sidebar.js";
+import {docsGetStartStruct} from "@components/render/sections/docs/get-start.js";
+import {docsCoreConceptStruct} from "@components/render/sections/docs/core-concepts.js";
+import {docsHowUseStruct} from "@components/render/sections/docs/how-use.js";
+
+const docSection = {
+    "get-start": docsGetStartStruct,
+    "core-concepts": docsCoreConceptStruct,
+    "how-use": docsHowUseStruct
+};
+
+const docsViewStruct = (section) => {
+    let schema = {
+        element: "section",
+        attr: {
+            class: "flex flex-col flex-wrap grow p-2 pl-16",
+        }
+    };
+    if(docSection[section]) schema['children'] = [docSection[section]()];
+
+    return {
+        element: "section",
+        attr: {
+            class: "relative flex flex-col grow min-h-fit overflow-auto will-change-scroll",
+        },
+        children: [
+            sidebarStruct(),
+            schema
+        ]
+    }
+};
+
+const struct = (section) => {
+    return [docsViewStruct(section)]
+}
+
+export const docStruct = Object.freeze(struct);
